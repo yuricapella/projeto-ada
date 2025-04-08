f21e46e266135b2ab078d7035ac8a13d0b2ad701
Yuri Capella <96320879+yuricapella@users.noreply.github.com>
2025-04-07 21:54:50 -0300

    feat: adiciona pagina do projeto logica de programacao inicial

---
+++ src/main/java/tech/ada/projeto_ada/logica_programacao/util/TabelaDescontosImpostoDeRenda.java
@@ -0,0 +1,34 @@
+package tech.ada.projeto_ada.logica_programacao.util;
+
+public enum TabelaDescontosImpostoDeRenda {
+    FAIXA_SALARIAL_1(1903.98,0),
+    FAIXA_SALARIAL_2(2826.65,7.5),
+    FAIXA_SALARIAL_3(3751.05,15),
+    FAIXA_SALARIAL_4(4664.68,22.5),
+    FAIXA_SALARIAL_5(Double.MAX_VALUE,27.5);
+
+    private final double limite;
+    private final double desconto;
+
+    TabelaDescontosImpostoDeRenda(double limite, double desconto) {
+        this.limite = limite;
+        this.desconto = desconto;
+    }
+
+    public double obterValorLimite() {
+        return limite;
+    }
+
+    public double obterDescontoPercentual() {
+        return desconto;
+    }
+
+    public static double obterDescontoPercentual(double salarioBruto){
+        for (TabelaDescontosImpostoDeRenda faixa : TabelaDescontosImpostoDeRenda.values()) {
+            if (salarioBruto <= faixa.obterValorLimite()) {
+                return faixa.obterDescontoPercentual();
+            }
+        }
+        return 0;
+    }
+}
