78414e3d01ce4f51818185bf7209c4bc15da7628
Yuri Capella <96320879+yuricapella@users.noreply.github.com>
2025-05-10 14:51:48 -0300

    feat(poo1): adicionar páginas HTML e controllers para home e listagem de clientes

--- src/main/java/tech/ada/projeto_ada/poo1/controller/ClienteViewController.java
+++ src/main/java/tech/ada/projeto_ada/poo1/controller/ClienteViewController.java
@@ -1,26 +1,28 @@
 package tech.ada.projeto_ada.poo1.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import tech.ada.projeto_ada.poo1.dto.ClienteResponseDTO;
 import tech.ada.projeto_ada.poo1.service.cliente.BuscarClienteService;
 
 import java.util.List;
 
-@RestController
-@RequestMapping(path = "/api/poo1/clientes")
-public class ClienteApiController {
+@Controller
+@RequestMapping("poo1/clientes")
+public class ClienteViewController {
+
     private final BuscarClienteService buscarClienteService;
 
-    public ClienteApiController(BuscarClienteService buscarClienteService) {
+    public ClienteViewController(BuscarClienteService buscarClienteService) {
         this.buscarClienteService = buscarClienteService;
     }
 
     @GetMapping
-    public List<ClienteResponseDTO> buscarTodosClientes(){
-        return buscarClienteService.buscarTodosClientes();
+    public String listarClientes(Model model) {
+        List<ClienteResponseDTO> clientes = buscarClienteService.buscarTodosClientes();
+        model.addAttribute("clientes", clientes);
+        return "poo1/clientes";
     }
-
-
 }
