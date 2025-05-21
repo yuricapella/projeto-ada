bdd9e8c32ef06028d39c5c991f2008c8505c765a
Yuri Capella <96320879+yuricapella@users.noreply.github.com>
2025-05-21 15:47:02 -0300

    feat(SeleniumTestUtil): adiciona classe utilitária para metodos padrões.

---
+++ src/test/java/tech/ada/projeto_ada/selenium/util/SeleniumTestUtil.java
@@ -0,0 +1,44 @@
+package tech.ada.projeto_ada.selenium.util;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SeleniumTestUtil {
+
+
+    public static void fazerLogin(WebDriver driver, WebDriverWait wait) {
+        driver.get("http://localhost:8080/login");
+        driver.findElement(By.name("email")).sendKeys("joao.silva@email.com");
+        driver.findElement(By.name("senha")).sendKeys("senha123");
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+        wait.until(ExpectedConditions.urlToBe("http://localhost:8080/home"));
+    }
+
+    public static void acessarPaginaInicialLocacaoVeiculos(WebDriver driver, WebDriverWait wait){
+        driver.findElement(By.id("btn-locacao")).click();
+        wait.until(ExpectedConditions.urlToBe("http://localhost:8080/poo1/home"));
+    }
+
+    public static void clicarBotao(WebDriverWait wait, String id){
+        WebElement button = wait.until(ExpectedConditions.elementToBeClickable(By.id(id)));
+        button.click();
+    }
+
+    public static void preencherCampo(WebDriverWait wait, String id, String valor){
+        WebElement input = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id(id)));
+        input.sendKeys(valor);
+    }
+
+    public static void selecionarOpcaoPorTexto(WebDriverWait wait, WebDriver driver, String id, String valor){
+        Select selectDocumento = new Select(driver.findElement(By.id(id)));
+        selectDocumento.selectByVisibleText(valor);
+    }
+
+    public static void esperarUrl(WebDriverWait wait, String url) {
+        wait.until(ExpectedConditions.urlToBe(url));
+    }
+}
